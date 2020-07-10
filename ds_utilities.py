afc1229f08122276b2679cbcebcd33f21b5350b2
RAV10K1 <ravindra.tennekone@gmail.com>
2020-07-09 18:22:06 -0700

    added some scripts

---
+++ ds_utilities.py
@@ -0,0 +1,9 @@
+def enlarge(n):
+    """This function will multiply the input by 100
+    
+    Args:
+        x ([type]): [description]
+    """
+    return n*100
+y = int(input("Choose a number: "))
+print(y, enlarge(y))
